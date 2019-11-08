ba5f74ee7c0c818d22cd485ed0839bd9b0a91713
Roy Wakumelo Jr <roywakumelo@roys-mbp.attlocal.net>
2019-11-08 07:06:21 -0500

    Migration functions complete

--- data/migrations/20191108063700_recipes.js
+++ data/migrations/20191108063700_recipes.js
@@ -1,8 +1,61 @@
 
 exports.up = function(knex) {
-  
+    exports.up = function(knex) {
+        return knex.schema
+        .createTable('recipes', tbl => {
+            tbl.increments();
+            tbl.string('name').notNullable()
+            tbl.integer('servings').notNullable()
+            tbl.string('duration').notNullable()
+    
+        })
+
+        .createTable('ingredients_list', tbl => {
+            tbl.increments();
+            tbl.string('name').notNullable()
+        })
+
+        .createTable('recipe_ingredients', tbl => {
+            tbl.increments();
+            tbl.unique(['recipe_id', 'ingredients_id'])
+
+            tbl.integer('recipe_id')
+            .unsigned()
+            .notNullable()
+            .references('id')
+            .inTable('recipes')
+            .onUpdate('CASCADE')
+            .onDelete('CASCADE')
+
+            tbl.integer('ingredients_id')
+            .unsigned()
+            .notNullable()
+            .references('id')
+            .inTable('recipes')
+            .onUpdate('CASCADE')
+            .onDelete('CASCADE');
+        })
+
+        .createTable('instructions', tbl => {
+            tbl.increments();
+            tbl.string('instructions').notNullable()
+            
+            tbl.integer('recipe_id')
+            .unsigned()
+            .notNullable()
+            .references('id')
+            .inTable('recipes')
+            .onUpdate('CASCADE')
+            .onDelete('CASCADE')
+
+        })
+    };    
 };
 
 exports.down = function(knex) {
-  
+    return knex.schema
+    .droTableIfExists('recipes')
+    .droTableIfExists('ingredients_list')
+    .droTableIfExists('recipe_ingredients')
+    .droTableIfExists('instructions')
 };
